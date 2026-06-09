9cb1624595b10d6610a914e5e1ca075ad9a2444d
Azharelahi <143085056+Azharelahi@users.noreply.github.com>
2026-06-09 15:42:46 +0500

    added dummy component for the frontend usage.

---
+++ src/components/dummyComp.jsx
@@ -0,0 +1,8 @@
+export default function DummyComp() {
+    return (
+        <div>
+            <h1>Dummy Component</h1>
+            <p>This is a dummy component for testing purposes.</p>
+        </div>
+    )
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,11 @@
+import DummyComp from "../components/dummyComp";
+
 export default function Dashboard() {
   return (
     <div>
       <h1>Family Dental Clinic</h1>
       <p>Dashboard Page</p>
+      <DummyComp/>
     </div>
   );
 }
